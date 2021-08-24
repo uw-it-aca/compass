3fe5c9981c5fac9e97b547b706dba2166dd91efe
Nick Falco <ncfalco@gmail.com>
2021-08-24 15:41:31 -0500

    Initial commit

---
+++ app_name_vue/router/index.js
@@ -0,0 +1,26 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+// page components
+import Home from '../pages/home.vue';
+import Customize from '../pages/customize.vue';
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/customize",
+    name: "Customize",
+    component: Customize,
+    pathToRegexpOptions: { strict: true }
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
