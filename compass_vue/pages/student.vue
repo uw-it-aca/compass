4fea079c17e37fde6017b531ce3c450b3fef8e06
Charlon Palacay <charlon@gmail.com>
2021-09-09 09:07:03 -0700

    Adds mock student photo.

--- compass_vue/pages/student.vue
+++ compass_vue/pages/student.vue
@@ -26,14 +26,15 @@
         <div class="row">
           <div class="col-sm-8">
             <div class="card mb-3">
-              <div class="row g-0">
-                <div class="col-sm-4">
-                  <svg class="bd-placeholder-img img-fluid rounded-start" width="100%" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Student Photo" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Student Photo</text></svg>
+              <div class="row">
+                <div class="col-4">
+                  <img src="/static/img/napoleon_dynamite.jpeg" class="img-fluid rounded-start" alt="">
                 </div>
-                <div class="col-sm-8">
+                <div class="col-8">
                   <div class="card-body">
                     <h3 class="card-title">Profile</h3>
                     <ul>
+                      <li>StudentID Photo</li>
                       <li>Preferred Name</li>
                       <li>Pronouns</li>
                       <li>UW NetId</li>
