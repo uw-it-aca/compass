e959ada22a3728b637ae33c6c0a458db5bef11ef
Charlon Palacay <charlon@gmail.com>
2021-09-20 10:36:26 -0700

    Fixes formatting.

--- docker/settings.py
+++ docker/settings.py
@@ -1,12 +1,9 @@
 from .base_settings import *
 
-INSTALLED_APPS += [
-    'webpack_bridge',
-    'compass.apps.CompassConfig'
-]
+INSTALLED_APPS += ["webpack_bridge", "compass.apps.CompassConfig"]
 
 STATICFILES_DIRS = [
-    '/static/compass/',
+    "/static/compass/",
 ]
 
 DATA_ROOT = os.path.join(BASE_DIR, "compass/data")
@@ -14,16 +11,18 @@ DATA_ROOT = os.path.join(BASE_DIR, "compass/data")
 GOOGLE_ANALYTICS_KEY = os.getenv("GOOGLE_ANALYTICS_KEY", default="")
 LOGOUT_URL = os.getenv("LOGOUT_URL", default="")
 
-TEMPLATES[0]['OPTIONS']['context_processors'].extend([
-    'compass.context_processors.google_analytics',
-    'compass.context_processors.logout_url',
-    'compass.context_processors.django_debug'
-])
+TEMPLATES[0]["OPTIONS"]["context_processors"].extend(
+    [
+        "compass.context_processors.google_analytics",
+        "compass.context_processors.logout_url",
+        "compass.context_processors.django_debug",
+    ]
+)
 
-DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 if os.getenv("ENV") == "localdev":
     DEBUG = True
-    COMPASS_USERS_GROUP = 'u_test_group'
+    COMPASS_USERS_GROUP = "u_test_group"
 else:
-    COMPASS_USERS_GROUP = os.getenv('ACCESS_GROUP', '')
+    COMPASS_USERS_GROUP = os.getenv("ACCESS_GROUP", "")
