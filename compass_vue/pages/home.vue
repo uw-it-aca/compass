76dc6e3bc3642c518a8a8b1f36dcca22254eb030
Charlon Palacay <charlon@gmail.com>
2021-09-30 15:24:43 -0700

    Moves netid and user gender into main table cell.

--- compass_vue/pages/home.vue
+++ compass_vue/pages/home.vue
@@ -11,7 +11,7 @@
       <div class="row justify-content-between mt-5 mb-2">
         <div class="col-sm-4">
           <div class="small">
-            <div class="d-inline me-3 text-nowrap">Find student by here:</div>
+            <div class="d-inline me-3 text-nowrap">Find student by:</div>
             <div class="d-inline text-nowrap">
               <div class="form-check form-check-inline" v-for="option in searchRadioOptions" :key="option.id">
                 <input class="form-check-input" type="radio" name="searchRadioOptions" :id="option.id" :value="option" v-model="searchOption">
@@ -42,42 +42,42 @@
                 <th scope="col"></th>
                 <th scope="col">Student Name</th>
                 <th scope="col">Student Number</th>
-                <th scope="col">UW NetID</th>
                 <th scope="col">Class</th>
                 <th scope="col">Major</th>
                 <th scope="col">Enroll Status</th>
-                <th scope="col">Gender</th>
                 <th scope="col">Adviser</th>
               </tr>
             </thead>
             <tbody>
               <tr v-for="(item,) in students" :key="item.SystemKey">
                 <td>
-                  <div style="width:40px;">
+                  <div style="width:45px;">
                     <img src="/static/img/napoleon-dynamite.jpeg" class="img-fluid rounded-circle border border-3" alt="">
                   </div>
                 </td>
-                <td>{{item.student_name}}</td>
+                <td>
+                  <div>{{item.student_name}} - {{item.gender}}</div>
+                  <div class="small">{{item.uw_net_id}}</div>
+                </td>
                 <td>
                   <router-link :to="{ name: 'Student', params: { id: item.student_number } }">
                     {{item.student_number}}
                   </router-link>
                 </td>
-                <td>{{item.uw_net_id}}</td>
                 <td>{{item.class_desc}}</td>
                 <td>{{item.major_full_name}}</td>
                 <td>{{item.enrollment_status_desc}}</td>
-                <td>{{item.gender}}</td>
                 <td>{{item.adviser_full_name}}</td>
               </tr>
             </tbody>
           </table>
         </div>
       </div>
-      <div class="row">
-        <div class="col">
+      <div class="row justify-content-md-center">
+        <div class="col-6 justify-content-md-center text-center">
           <paginate
             v-model="currentPage"
+            :theme="bootstrap4"
             :records="studentsCount"
             :per-page="pageSize"
             @paginate="loadStudentList">
