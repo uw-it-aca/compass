3ff6cb87fb1c019c58a6d353391d70f822292ac4
Charlon Palacay <charlon@gmail.com>
2022-06-06 14:03:24 -0700

    Implement authorization guards as a route meta setting.

--- compass_vue/router/index.js
+++ compass_vue/router/index.js
@@ -8,6 +8,14 @@ import Student from "../pages/student.vue";
 import Reports from "../pages/reports.vue";
 import Settings from "../pages/settings.vue";
 
+// MARK: user roles
+export const Role = {
+  Super: "Super",
+  Admin: "Admin",
+  Support: "Support",
+  User: "User",
+};
+
 const routes = [
   {
     path: "/",
@@ -17,56 +25,31 @@ const routes = [
   },
   {
     path: "/caseload",
-    name: "Caseload",
     component: Caseload,
     pathToRegexpOptions: { strict: true },
     props: true,
   },
   {
     path: "/student/:id?",
-    name: "Student",
     component: Student,
     pathToRegexpOptions: { strict: true },
     props: true,
   },
   {
     path: "/reports",
-    name: "Reports",
     component: Reports,
+    meta: { authorize: [Role.Support] },
     pathToRegexpOptions: { strict: true },
     props: true,
-    beforeEnter(to, from, next) {
-      if (hasPermissions()) {
-        // got to reports page
-        next();
-      } else {
-        // redirect to 'not authorized' page in django
-        window.location.replace("/not-authorized");
-      }
-    },
   },
   {
     path: "/settings",
-    name: "Settings",
     component: Settings,
+    meta: { authorize: [Role.Super, Role.Admin] },
     pathToRegexpOptions: { strict: true },
     props: true,
-    beforeEnter(to, from, next) {
-      if (hasPermissions()) {
-        // got to reports page
-        next();
-      } else {
-        // redirect to 'not authorized' page in django
-        window.location.replace("/not-authorized");
-      }
-    },
   },
 ];
-
-function hasPermissions() {
-  // get user permssions from /api/endpoint
-  return true;
-}
 
 const router = createRouter({
   history: createWebHistory(),
@@ -78,6 +61,25 @@ const router = createRouter({
       return { top: 0 };
     }
   },
+});
+
+// MARK: implement route guards
+router.beforeEach((to, from, next) => {
+  // get the authorization settings from the meta field for the route
+  const { authorize } = to.meta;
+
+  // TODO: get the authenticated users roles from api/endpoint
+  let currentUser = { role: Role.User };
+
+  // check if authorization is required for this route
+  if (authorize) {
+    // check to see if current user's role is authorized to view page
+    if (authorize.length && !authorize.includes(currentUser.role)) {
+      // redirect to 'not authorized' page in django
+      window.location.replace("/not-authorized");
+    }
+  }
+  next();
 });
 
 // add router tracking to vue-gtag-next
