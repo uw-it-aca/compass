9a15800895bb0798a02fee7ed3db7cede5d2fefc
Charlon Palacay <charlon@gmail.com>
2022-06-07 10:22:23 -0700

    Fixes formatting.

--- compass_vue/components/student/visits.vue
+++ compass_vue/components/student/visits.vue
@@ -5,48 +5,48 @@
     </template>
     <template #body>
       <div class="table-responsive">
-              <table class="table table-hover table-striped m-0">
-                <thead class="small">
-                  <tr>
-                    <th>Date</th>
-                    <th>Course</th>
-                    <th>Check-in</th>
-                    <th>Check-out</th>
-                    <th>Duration</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td scope="row">09/23/2020</td>
-                    <td>BUS 220</td>
-                    <td>10:35am</td>
-                    <td>11:42pm</td>
-                    <td>1hr 2mins</td>
-                  </tr>
-                  <tr>
-                    <td scope="row">07/04/2020</td>
-                    <td>CSE 142</td>
-                    <td>9:15am</td>
-                    <td>11:2am</td>
-                    <td>2 hrs</td>
-                  </tr>
-                  <tr>
-                    <td scope="row">06/29/2020</td>
-                    <td>BUS 220</td>
-                    <td>4:31pm</td>
-                    <td>4:45pm</td>
-                    <td>15 mins</td>
-                  </tr>
-                  <tr>
-                    <td scope="row">5/14/2020</td>
-                    <td>BUS 220</td>
-                    <td>2:34pm</td>
-                    <td>6:35pm</td>
-                    <td>4 hrs</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+        <table class="table table-hover table-striped m-0">
+          <thead class="small">
+            <tr>
+              <th>Date</th>
+              <th>Course</th>
+              <th>Check-in</th>
+              <th>Check-out</th>
+              <th>Duration</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td scope="row">09/23/2020</td>
+              <td>BUS 220</td>
+              <td>10:35am</td>
+              <td>11:42pm</td>
+              <td>1hr 2mins</td>
+            </tr>
+            <tr>
+              <td scope="row">07/04/2020</td>
+              <td>CSE 142</td>
+              <td>9:15am</td>
+              <td>11:2am</td>
+              <td>2 hrs</td>
+            </tr>
+            <tr>
+              <td scope="row">06/29/2020</td>
+              <td>BUS 220</td>
+              <td>4:31pm</td>
+              <td>4:45pm</td>
+              <td>15 mins</td>
+            </tr>
+            <tr>
+              <td scope="row">5/14/2020</td>
+              <td>BUS 220</td>
+              <td>2:34pm</td>
+              <td>6:35pm</td>
+              <td>4 hrs</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </template>
   </axdd-card>
 </template>
