251ef6167d0a909d40f12909cf7f3fb8daf08dd4
Charlon Palacay <charlon@gmail.com>
2022-08-03 10:09:41 -0700

    Updates NavMenu and NavMessage components.

---
+++ compass_vue/components/nav-message.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="text-light">
+    <p
+      v-show="userRole === 'manager'"
+      class="text-light-gray bg-dark-purple rounded-3 p-3 small"
+    >
+      Your role <strong>{{ userRole }}</strong
+      >... go forth and do admin related things!
+    </p>
+
+    <p
+      v-show="$route.path == '/'"
+      class="text-light-gray bg-dark-purple rounded-3 p-3 small"
+    >
+      Welcome to <strong>home</strong>... Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Cum voluptatibus, laborum inventore magni,
+      dolore quod incidunt error reiciendis nesciunt ex atque, amet culpa quia
+      ullam sit. Deleniti id at odit!
+    </p>
+
+    <p
+      v-show="$route.path == '/caseload'"
+      class="text-light-gray bg-dark-purple rounded-3 p-3 small"
+    >
+      Welcome to <strong>caseloads</strong>... Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Cum voluptatibus, laborum inventore magni,
+      dolore quod incidunt error reiciendis nesciunt ex atque, amet culpa quia
+      ullam sit. Deleniti id at odit!
+    </p>
+  </div>
+</template>
