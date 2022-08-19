a6796dbb102eba80c7832281e9706a8bda14c456
Nicholas Falco <ncfalco@uw.edu>
2022-08-19 13:04:08 -0500

    Handle error pages in Vue router not Django urls

---
+++ compass_vue/pages/page-not-authorized.vue
@@ -0,0 +1,25 @@
+// reports.vue
+
+<template>
+  <layout :page-title="pageTitle">
+    <!-- page content -->
+    <template #content>403 Forbidden</template>
+  </layout>
+</template>
+
+<script>
+import Layout from "../layout.vue";
+
+export default {
+  components: {
+    layout: Layout,
+  },
+  data() {
+    return {
+      pageTitle: "403 Forbidden",
+    };
+  },
+};
+</script>
+
+<style lang="scss"></style>
--- compass_vue/router/index.js
+++ compass_vue/router/index.js
@@ -7,6 +7,8 @@ import Caseload from "../pages/caseload.vue";
 import Student from "../pages/student.vue";
 import Reports from "../pages/reports.vue";
 import Settings from "../pages/settings.vue";
+import PageNotFound from "../pages/page-not-found.vue";
+import NotAuthorized from "../pages/page-not-authorized.vue";
 
 // MARK: user roles
 export const Role = {
@@ -20,37 +22,45 @@ const routes = [
   {
     path: "/",
     component: CheckIn,
-    meta: { authorize: [Role.Manager, Role.User] },
+    meta: { authorize: [Role.Admin, Role.Manager, Role.User] },
     pathToRegexpOptions: { strict: true },
     props: true,
   },
   {
     path: "/caseload/:id?",
     component: Caseload,
-    meta: { authorize: [Role.Manager, Role.User] },
+    meta: { authorize: [Role.Admin, Role.Manager, Role.User] },
     pathToRegexpOptions: { strict: true },
     props: true,
   },
   {
     path: "/student/:id?",
     component: Student,
-    meta: { authorize: [Role.Manager, Role.User] },
+    meta: { authorize: [Role.Admin, Role.Manager, Role.User] },
     pathToRegexpOptions: { strict: true },
     props: true,
   },
   {
     path: "/reports",
     component: Reports,
-    meta: { authorize: [Role.Manager] },
+    meta: { authorize: [Role.Admin, Role.Manager] },
     pathToRegexpOptions: { strict: true },
     props: true,
   },
   {
     path: "/settings",
     component: Settings,
-    meta: { authorize: [Role.Manager] },
+    meta: { authorize: [Role.Admin, Role.Manager] },
     pathToRegexpOptions: { strict: true },
     props: true,
+  },
+  {
+    path: "/not-authorized",
+    component: NotAuthorized,
+  },
+  {
+    path: "/:catchAll(.*)", // HTTP 404 Error
+    component: PageNotFound,
   },
 ];
 
@@ -73,14 +83,13 @@ router.beforeEach((to, from, next) => {
 
   // get the authenticated user role from django context
   let userRole = document.body.getAttribute("data-user-role");
-  // console.log(userRole);
 
   // check if authorization is required for this route
   if (authorize) {
     // check to see if current user's role is authorized to view page
     if (authorize.length && !authorize.includes(userRole)) {
       // redirect to 'not authorized' page in django
-      window.location.replace("/not-authorized");
+      next("/not-authorized");
     }
   }
   next();
