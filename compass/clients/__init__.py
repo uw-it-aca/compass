ca7c08ef980e3314832734b9babdc2c88bdec034
Nicholas Falco <ncfalco@uw.edu>
2022-08-30 16:31:13 -0500

    Fix typo

--- compass/clients/__init__.py
+++ compass/clients/__init__.py
@@ -4,7 +4,7 @@
 import os
 
 if os.environ.get("AXDD_PERSON_CLIENT_ENV") == "PROD":
-    from compass.cients.person_client import CompassPersonClient
+    from compass.clients.person_client import CompassPersonClient
 else:
     from compass.clients.mock_person_client import \
         MockedCompassPersonClient as CompassPersonClient  # noqa
