1efcb5e5b5b68ac634ab8cc788c43710dce1f3cb
Jim Laney <jlaney@uw.edu>
2022-08-31 22:33:43 -0700

    use login_required

--- compass/views/api/__init__.py
+++ compass/views/api/__init__.py
@@ -1,14 +1,14 @@
 # Copyright 2022 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-from compass.decorators import verify_access
 from compass.models import AccessGroup
 from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from rest_framework.generics import GenericAPIView
 from uw_saml.utils import get_attribute
 
 
-@method_decorator(verify_access(), name='dispatch')
+@method_decorator(login_required, name='dispatch')
 class BaseAPIView(GenericAPIView):
 
     def get_access_groups(self, request):
