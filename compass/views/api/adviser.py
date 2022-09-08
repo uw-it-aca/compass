1431eac978f334e6179f51979a246e5c9c3b7b77
Nicholas Falco <ncfalco@uw.edu>
2022-09-08 13:02:07 -0500

    Don't include photos in adviser caseload

--- compass/views/api/adviser.py
+++ compass/views/api/adviser.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from compass.views.api import BaseAPIView
-from compass.dao.photo import PhotoDAO
 from compass.clients import CompassPersonClient
 from compass.models import Contact
 from compass.serializers import ContactReadSerializer
@@ -23,12 +22,10 @@ class AdviserContactsView(BaseAPIView):
         contact_queryset = Contact.objects.filter(
             author__uwnetid=adviser_netid).all()
         contacts = []
-        photo_dao = PhotoDAO()
         for contact in contact_queryset:
             contact_dict = ContactReadSerializer(contact, many=False).data
             person = client.get_person_by_system_key(
                 contact.student.system_key)
-            person.photo_url = photo_dao.get_photo_url(person.uwregid)
             contact_dict["student"] = person.to_dict()
             contacts.append(contact_dict)
         return JsonResponse([contact for contact in contacts], safe=False)
@@ -47,8 +44,5 @@ class AdviserCaseloadView(BaseAPIView):
             persons = client.get_adviser_caseload(adviser_netid)
         except AdviserNotFoundException:
             persons = []
-        photo_dao = PhotoDAO()
-        for person in persons:
-            person.photo_url = photo_dao.get_photo_url(person.uwregid)
         return JsonResponse([person.to_dict() for person in persons],
                             safe=False)
