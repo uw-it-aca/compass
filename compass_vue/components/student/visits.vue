d447d714af0cdc07ec8b8ec0a42da2ef98d6ea2f
Charlon Palacay <charlon@gmail.com>
2022-09-15 19:38:44 -0700

    Initial styling of IC visits.

--- compass_vue/components/student/visits.vue
+++ compass_vue/components/student/visits.vue
@@ -6,48 +6,33 @@
       >
     </template>
     <template #body>
-      <div class="table-responsive m-n3">
-        <table class="table m-0">
-          <thead class="table-light text-muted small">
-            <tr>
-              <th class="ps-3">Date</th>
-              <th>Course</th>
-              <th>Check-in</th>
-              <th>Check-out</th>
-              <th>Duration</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td scope="row" class="ps-3">09/23/2020</td>
-              <td>BUS 220</td>
-              <td>10:35am</td>
-              <td>11:42pm</td>
-              <td>1hr 2mins</td>
-            </tr>
-            <tr>
-              <td scope="row" class="ps-3">07/04/2020</td>
-              <td>CSE 142</td>
-              <td>9:15am</td>
-              <td>11:2am</td>
-              <td>2 hrs</td>
-            </tr>
-            <tr>
-              <td scope="row" class="ps-3">06/29/2020</td>
-              <td>BUS 220</td>
-              <td>4:31pm</td>
-              <td>4:45pm</td>
-              <td>15 mins</td>
-            </tr>
-            <tr>
-              <td scope="row" class="ps-3">5/14/2020</td>
-              <td>BUS 220</td>
-              <td>2:34pm</td>
-              <td>6:35pm</td>
-              <td>4 hrs</td>
-            </tr>
-          </tbody>
-        </table>
+      <div class="d-flex">
+        <div class="vr text-purple" aria-hidden="true"></div>
+        <ul class="list-unstyled mb-0" style="margin-left: -6px; z-index: 1">
+          <li class="d-flex mb-2">
+            <i class="bi bi-circle-fill text-purple fs-10 mt-1 me-2"></i>
+            <span
+              >09/23/2020, BUS 220 <br />
+              10:35am - 11:42am</span
+            >
+          </li>
+          <li class="d-flex mb-2">
+            <i class="bi bi-circle-fill text-purple fs-10 mt-1 me-2"></i>
+            <span>09/23/2020, CSE 143<br />10:35am - 11:42am</span>
+          </li>
+          <li class="d-flex mb-2">
+            <i class="bi bi-circle-fill text-purple fs-10 mt-1 me-2"></i>
+            <span>09/23/2020, CSE 143<br />10:35am - 11:42am</span>
+          </li>
+          <li class="d-flex mb-2">
+            <i class="bi bi-circle-fill text-purple fs-10 mt-1 me-2"></i>
+            <span>09/23/2020, CSE 143<br />10:35am - 11:42am</span>
+          </li>
+          <li class="d-flex">
+            <i class="bi bi-circle-fill text-purple fs-10 mt-1 me-2"></i>
+            <span>09/23/2020, CSE 143<br />10:35am - 11:42am</span>
+          </li>
+        </ul>
       </div>
     </template>
   </axdd-card>
