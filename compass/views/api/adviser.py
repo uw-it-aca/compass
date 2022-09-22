cc7f010168d33823e56de31c2d9de247f9713053
Charlon Palacay <charlon@gmail.com>
2022-09-22 16:57:26 -0700

    Updates photo size for caseloads.

--- compass/views/api/adviser.py
+++ compass/views/api/adviser.py
@@ -12,32 +12,37 @@ from uw_person_client.exceptions import AdviserNotFoundException
 
 
 class AdviserContactsView(BaseAPIView):
-    '''
+    """
     API endpoint returning a list of contacts of an adviser
     /api/internal/adviser/<adviser_netid>/contact/
-    '''
+    """
+
     def get(self, request, adviser_netid):
         client = UWPersonClient()
         contact_queryset = Contact.objects.filter(
-            author__uwnetid=adviser_netid).all()
+            author__uwnetid=adviser_netid
+        ).all()
         contacts = []
         photo_dao = PhotoDAO()
         for contact in contact_queryset:
             contact_dict = ContactReadSerializer(contact, many=False).data
             person = client.get_person_by_system_key(
-                contact.student.system_key)
+                contact.student.system_key
+            )
             person.photo_url = photo_dao.get_photo_url(
-                person.uwregid, "medium")
+                person.uwregid, "medium"
+            )
             contact_dict["student"] = person.to_dict()
             contacts.append(contact_dict)
         return JsonResponse([contact for contact in contacts], safe=False)
 
 
 class AdviserCaseloadView(BaseAPIView):
-    '''
+    """
     API endpoint returning a list of contacts of an adviser
     /api/internal/adviser/<adviser_netid>/caseload/
-    '''
+    """
+
     def get(self, request, adviser_netid):
         client = CompassPersonClient()
         try:
@@ -46,6 +51,9 @@ class AdviserCaseloadView(BaseAPIView):
             persons = []
         photo_dao = PhotoDAO()
         for person in persons:
-            person.photo_url = photo_dao.get_photo_url(person.uwregid)
-        return JsonResponse([person.to_dict() for person in persons],
-                            safe=False)
+            person.photo_url = photo_dao.get_photo_url(
+                person.uwregid, "medium"
+            )
+        return JsonResponse(
+            [person.to_dict() for person in persons], safe=False
+        )
