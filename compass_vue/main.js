b26d2102d40c7bd7af783003fe05103aad67e551
Charlon Palacay <charlon@gmail.com>
2022-09-28 22:05:26 -0700

    Updates vue-gtag and fixes css.

--- compass_vue/main.js
+++ compass_vue/main.js
@@ -3,7 +3,7 @@ import App from "./app.vue";
 import router from "./router";
 import store from "./store";
 
-import VueGtag from "vue-gtag-next";
+import VueGtag from "vue-gtag";
 import { Vue3Mq, MqResponsive } from "vue3-mq";
 
 // bootstrap js
@@ -20,22 +20,31 @@ const debugMode = document.body.getAttribute("data-django-debug");
 
 app.config.productionTip = false;
 
-// vue-gtag-next
-app.use(VueGtag, {
-  isEnabled: debugMode == "false",
-  property: {
-    id: gaCode,
-    params: {
-      anonymize_ip: true,
+// vue-router
+app.use(router);
+
+// vue-gtag (w/ vue-router auto-tracking)
+app.use(
+  VueGtag,
+  {
+    isEnabled: debugMode == "false",
+    property: {
+      id: gaCode,
+      params: {
+        anonymize_ip: true,
+      },
     },
   },
-});
+  router
+);
 
 // vue-mq (media queries)
 app.use(Vue3Mq, {
   preset: "bootstrap5",
 });
 app.component("mq-responsive", MqResponsive);
-app.use(router);
+
+// vuex state management
 app.use(store);
+
 app.mount("#app");
