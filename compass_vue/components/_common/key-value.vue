6432d10b58be9aa3544d90883e169e3c4b8881bd
Leila Li <ml253@uw.edu>
2022-10-07 11:26:18 -0700

    error

--- compass_vue/components/_common/key-value.vue
+++ compass_vue/components/_common/key-value.vue
@@ -1,24 +1,29 @@
 <template>
-   <div :class="[mq.lgMinus ? 'justify-content-between' : '']" class="d-flex">
-     <div v-if="$slots['key']" :class="[variant == 'address' ? 'text-danger' : '']" class="fw-bold text-nowrap me-2">
-       <slot name="key"></slot>
-     </div>
-     <div v-if="$slots['value']" class="text-nowrap">
-       <slot name="value"></slot>
-     </div>
-   </div>
- </template>
+  <div :class="[mq.lgMinus ? 'justify-content-between' : '']" class="d-flex">
+    <div
+      v-if="$slots['key']"
+      :class="[variant == 'address' ? 'text-danger' : '']"
+      class="fw-bold text-nowrap me-2"
+    >
+      <slot name="key"></slot>
+    </div>
+    <div v-if="$slots['value']" class="text-nowrap">
+      <slot name="value"></slot>
+    </div>
+  </div>
+</template>
 
- <script>
- export default {
-   inject: ["mq"],
-   props: {
-     variant: {
-       type: String,
-       default: "standard",
-     },
-   },
-   data() {
-     return {};
-   },
- }
+<script>
+export default {
+  inject: ["mq"],
+  props: {
+    variant: {
+      type: String,
+      default: "standard",
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
