acce6a5d1dfbf6e352e304c23b5a9a40da1b0120
Leila Li <ml253@uw.edu>
2022-11-29 13:15:53 -0800

    spacing

--- compass_vue/pages/student.vue
+++ compass_vue/pages/student.vue
@@ -18,7 +18,7 @@
           </div>
         </div>
 
-        <div class="row px-2">
+        <div class="row">
           <div class="row py-2">
             <div class="col-8">
               Authorizes Release of Directory Information:
