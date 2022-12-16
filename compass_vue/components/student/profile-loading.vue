41e017cc6c5f37614e617c958fb4d1d94ff427c3
Charlon Palacay <charlon@gmail.com>
2022-12-15 16:28:28 -0800

    Updates loading margin.

--- compass_vue/components/student/profile-loading.vue
+++ compass_vue/components/student/profile-loading.vue
@@ -2,11 +2,11 @@
   <div class="bg-light rounded-3 border-0 px-0 py-4 m-0">
     <div class="row">
       <div class="col-xl-3 my-auto">
-        <div class="text-center mb-4">
-          <div class="me-2">
+        <div class="text-center mb-5">
+          <div>
             <div
               class="placeholder-glow d-inline-block rounded-circle bg-gray"
-              style="width: 120px; height: 120px"
+              style="width: 128px; height: 128px"
             >
               &nbsp;
             </div>
