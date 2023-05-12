28f1cb01bda06b0b3792bd374e9b08f053e0fbc2
Charlon Palacay <charlon@gmail.com>
2023-05-12 10:50:43 -0700

    Updates person client to 1.1.7

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         'UW-RestClients-PWS~=2.1',
         'UW-Django-SAML2~=1.7',
         'urllib3~=1.25',
-        'axdd-person-client>=1.1.5',
+        'axdd-person-client>=1.1.7',
         'python-dateutil~=2.8'
     ],
     license="Apache License, Version 2.0",
