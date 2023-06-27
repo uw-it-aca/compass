a5de0ea6288d15b4d7da9133696adeb03d17642f
Jim Laney <jlaney@uw.edu>
2023-06-27 09:28:15 -0700

    drop import

--- compass/dao/person.py
+++ compass/dao/person.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from uw_pws import PWS, InvalidNetID
-from uw_saml.utils
 from compass.clients import CompassPersonClient, PersonNotFoundException
 import re
 
