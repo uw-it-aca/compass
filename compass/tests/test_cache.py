18c932a02d9f315bef6006ea8dd5a73041de5e4b
Stephen De Vight <devights@uw.edu>
2023-11-15 16:00:55 -0800

    test cache

---
+++ compass/tests/test_cache.py
@@ -0,0 +1,31 @@
+# Copyright 2023 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+
+import re
+from django.test import TestCase
+from compass.cache import CompassRestclientCache
+
+
+class TestCache(TestCase):
+    cache = None
+
+    def setUp(self):
+        self.cache = CompassRestclientCache()
+
+    def test_cache_expiration_time(self):
+        photo_time = self.cache.get_cache_expiration_time('pws',
+                                                          '/idcard/v1/photo')
+        self.assertEqual(photo_time, 432000)
+
+        pws_time = self.cache.get_cache_expiration_time('pws', '/person/')
+        self.assertEqual(pws_time, 14400)
+
+        sws_time = self.cache.get_cache_expiration_time('sws', '/student/')
+        self.assertEqual(sws_time, 86400)
+
+        gws_time = self.cache.get_cache_expiration_time('gws', '/group/')
+        self.assertEqual(gws_time, 900)
+
+        other_time = self.cache.get_cache_expiration_time('other', '/other/')
+        self.assertEqual(other_time, 3600)
