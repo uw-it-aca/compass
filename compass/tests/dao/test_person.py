7d6b03160eef8bc7f2d659097e3bceb796076782
Stephen De Vight <devights@uw.edu>
2023-11-15 16:28:00 -0800

    test overriable uwnetid

--- compass/tests/dao/test_person.py
+++ compass/tests/dao/test_person.py
@@ -3,7 +3,9 @@
 
 
 from django.test import TestCase
-from compass.dao.person import is_overridable_uwnetid
+from compass.dao.person import is_overridable_uwnetid, valid_uwnetid, \
+    valid_uwregid, valid_student_number, valid_system_key, person_from_uwnetid
+from uw_pws import InvalidNetID
 
 
 class TestPerson(TestCase):
@@ -27,3 +29,21 @@ class TestPerson(TestCase):
 
         err_msg = is_overridable_uwnetid("javerage2")
         self.assertEqual(err_msg, "Current netid: javerage, Prior netid: ")
+
+    def test_validation(self):
+        self.assertTrue(valid_uwnetid("javerage"))
+        self.assertFalse(valid_uwnetid("1javerage"))
+
+        self.assertTrue(valid_uwregid("9136CCB8F66711D5BE060004AC494FFE"))
+        self.assertFalse(valid_uwregid("!@#$"))
+
+        self.assertTrue(valid_student_number("1033334"))
+        self.assertFalse(valid_student_number("javerage"))
+
+        self.assertTrue(valid_system_key("123456789"))
+        self.assertFalse(valid_system_key("javerage"))
+
+    def test_person_from_uwnetid(self):
+        self.assertRaises(InvalidNetID, person_from_uwnetid, "1javerage")
+        self.assertIsNotNone(person_from_uwnetid("javerage"))
+
