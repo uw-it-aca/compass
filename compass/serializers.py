a758fff9bc4397e6571a4fb25868c915dd7fde1d
Jim Laney <jlaney@uw.edu>
2024-01-05 14:32:55 -0800

    remove redundant source keyword

--- compass/serializers.py
+++ compass/serializers.py
@@ -23,7 +23,7 @@ import pytz
 
 
 class AppUserSerializer(serializers.ModelSerializer):
-    display_name = serializers.CharField(source='display_name', read_only=True)
+    display_name = serializers.CharField(read_only=True)
 
     class Meta:
         model = AppUser
