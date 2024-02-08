b782af6c378ead4c86ddd8776960650f5ab9dcfe
Charlon Palacay <charlon@gmail.com>
2024-02-08 00:05:02 -0800

    Adds pagination button logic.

---
+++ compass_vue/components/_solstice/pagination.vue
@@ -0,0 +1,79 @@
+<template>
+  <slot></slot>
+  <nav aria-label="Page navigation example">
+    <ul class="pagination pagination-sm justify-content-center">
+      <li class="page-item">
+        <a
+          class="page-link"
+          :class="currentPage == 1 ? 'disabled' : ''"
+          href="#"
+          role="button"
+          @click="$emit('changePage', currentPage - 1)"
+          >Previous</a
+        >
+      </li>
+      <template v-for="page in pages" :key="page">
+        <li class="page-item">
+          <a
+            class="page-link"
+            :class="page == currentPage ? 'active' : ''"
+            href="#"
+            role="button"
+            @click="$emit('changePage', page)"
+            >{{ page }}</a
+          >
+        </li>
+      </template>
+      <li class="page-item">
+        <a
+          class="page-link"
+          :class="currentPage == pageCount ? 'disabled' : ''"
+          href="#"
+          role="button"
+          @click="$emit('changePage', currentPage + 1)"
+          >Next</a
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
+<script>
+import { computed } from "vue";
+
+export default {
+  //note this emit back to parent
+  emits: ["changePage"],
+  props: {
+    //changed to just get the length of the list
+    itemCount: {
+      type: Number,
+      required: true,
+    },
+    itemsPerPage: {
+      type: Number,
+      default: 10,
+    },
+    currentPage: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup(props) {
+    const pageCount = computed(() =>
+      Math.ceil(props.itemCount / props.itemsPerPage)
+    );
+
+    const pages = computed(() => {
+      return Array.from({ length: pageCount.value }, (_, i) => i + 1);
+    });
+
+    return {
+      pageCount,
+      pages,
+    };
+  },
+  data() {
+    return {};
+  },
+};
+</script>
--- compass_vue/components/caseload-table-display.vue
+++ compass_vue/components/caseload-table-display.vue
@@ -10,82 +10,96 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="person in persons" :key="person.uwnetid">
-          <td class="ps-3">
-            <profile-mini :person="person"></profile-mini>
-          </td>
-          <td class="small">{{ person.student.class_desc }}</td>
-          <td class="small">{{ person.student.campus_desc }}</td>
-          <td class="small">
-            <ul class="list-inline d-flex m-0">
-              <li class="list-inline-item d-none">
-                Special Program:
-                <!-- show N/A when student isn't in any special program -->
+        <Pagination
+          :itemCount="persons.length"
+          :itemsPerPage="itemsPerPage"
+          :currentPage="currentPage"
+          @changePage="changePage"
+        >
+          <!-- MARK: add paginated template as a slot -->
+          <tr v-for="(person, index) in displayedItems" :key="index">
+            <td class="ps-3">
+              <profile-mini :person="person"></profile-mini>
+            </td>
+            <td class="small">{{ person.student.class_desc }}</td>
+            <td class="small">{{ person.student.campus_desc }}</td>
+            <td class="small">
+              <ul class="list-inline d-flex m-0">
+                <li class="list-inline-item d-none">
+                  Special Program:
+                  <!-- show N/A when student isn't in any special program -->
 
-                <span
-                  v-if="person.student.special_program_code === '0'"
-                  class="small badge rounded-pill border-0 px-2 py-1 mb-0 me-1 text-bg-secondary"
-                >
-                  N/A</span
-                >
-                <span v-else class="badge text-bg-success">
-                  {{ person.student.special_program_code }},
-                  {{ person.student.special_program_desc }}
-                </span>
-              </li>
-              <li class="list-inline-item flex-fill w-25">
-                <div>Degree</div>
-                <span
-                  class="small badge rounded-pill border-0 px-2 py-1 mb-0 me-1 text-bg-secondary"
-                >
-                  {{ getDegreeData(person).degree_desc }}</span
-                >
-              </li>
-              <li class="list-inline-item flex-fill w-25">
-                <div>Scholarship</div>
-                <span
-                  class="small badge rounded-pill border-0 px-2 py-1 mb-0 me-1"
-                  :class="
-                    getScholarshipData(person).scholarship_type == 3 ||
-                    getScholarshipData(person).scholarship_type == 4
-                      ? 'text-bg-danger'
-                      : 'text-bg-secondary'
-                  "
-                  >{{ getScholarshipData(person).scholarship_desc }}</span
-                >
-              </li>
-              <li class="list-inline-item flex-fill w-25">
-                <div>Registered</div>
-                <span
-                  class="small badge rounded-pill border-0 px-2 py-1 mb-0 me-1"
-                  :class="
-                    !person.student.registered_in_quarter
-                      ? 'text-bg-danger'
-                      : 'text-bg-secondary'
-                  "
-                >
-                  {{ translateTrueFalse(person.student.registered_in_quarter) }}
-                </span>
-              </li>
-              <li class="list-inline-item d-none">
-                Enrollment Status Code: {{ person.student.enroll_status_code }}
-              </li>
-              <li class="list-inline-item flex-fill w-25">
-                <div>Holds</div>
-                <span
-                  class="small badge rounded-pill border-0 px-2 py-1 mb-0 me-1"
-                  :class="
-                    person.student.registration_hold_ind
-                      ? 'text-bg-danger'
-                      : 'text-bg-secondary'
-                  "
-                >
-                  {{ translateTrueFalse(person.student.registration_hold_ind) }}
-                </span>
-              </li>
-            </ul>
-          </td>
-        </tr>
+                  <span
+                    v-if="person.student.special_program_code === '0'"
+                    class="small badge rounded-pill border-0 px-2 py-1 mb-0 me-1 text-bg-secondary"
+                  >
+                    N/A</span
+                  >
+                  <span v-else class="badge text-bg-success">
+                    {{ person.student.special_program_code }},
+                    {{ person.student.special_program_desc }}
+                  </span>
+                </li>
+                <li class="list-inline-item flex-fill w-25">
+                  <div>Degree</div>
+                  <span
+                    class="small badge rounded-pill border-0 px-2 py-1 mb-0 me-1 text-bg-secondary"
+                  >
+                    {{ getDegreeData(person).degree_desc }}</span
+                  >
+                </li>
+                <li class="list-inline-item flex-fill w-25">
+                  <div>Scholarship</div>
+                  <span
+                    class="small badge rounded-pill border-0 px-2 py-1 mb-0 me-1"
+                    :class="
+                      getScholarshipData(person).scholarship_type == 3 ||
+                      getScholarshipData(person).scholarship_type == 4
+                        ? 'text-bg-danger'
+                        : 'text-bg-secondary'
+                    "
+                    >{{ getScholarshipData(person).scholarship_desc }}</span
+                  >
+                </li>
+                <li class="list-inline-item flex-fill w-25">
+                  <div>Registered</div>
+                  <span
+                    class="small badge rounded-pill border-0 px-2 py-1 mb-0 me-1"
+                    :class="
+                      !person.student.registered_in_quarter
+                        ? 'text-bg-danger'
+                        : 'text-bg-secondary'
+                    "
+                  >
+                    {{
+                      translateTrueFalse(person.student.registered_in_quarter)
+                    }}
+                  </span>
+                </li>
+                <li class="list-inline-item d-none">
+                  Enrollment Status Code:
+                  {{ person.student.enroll_status_code }}
+                </li>
+                <li class="list-inline-item flex-fill w-25">
+                  <div>Holds</div>
+                  <span
+                    class="small badge rounded-pill border-0 px-2 py-1 mb-0 me-1"
+                    :class="
+                      person.student.registration_hold_ind
+                        ? 'text-bg-danger'
+                        : 'text-bg-secondary'
+                    "
+                  >
+                    {{
+                      translateTrueFalse(person.student.registration_hold_ind)
+                    }}
+                  </span>
+                </li>
+              </ul>
+            </td>
+          </tr>
+          <!-- END slot -->
+        </Pagination>
       </tbody>
     </table>
   </div>
@@ -93,12 +107,15 @@
 </template>
 
 <script>
+import { ref, computed } from "vue";
 import ProfileMini from "@/components/student/profile-mini.vue";
+import Pagination from "@/components/_solstice/pagination.vue";
 import { translateTrueFalse } from "@/utils/translations";
 
 export default {
   components: {
     "profile-mini": ProfileMini,
+    Pagination,
   },
   props: {
     adviserNetId: {
@@ -110,8 +127,28 @@ export default {
       required: true,
     },
   },
-  setup() {
+  setup(props) {
+    // basic setup for pagination component
+    const currentPage = ref(1);
+    const itemsPerPage = ref(1); // set ref to number to be shown
+
+    const displayedItems = computed(() => {
+      const startIndex = (currentPage.value - 1) * itemsPerPage.value;
+      const endIndex = startIndex + itemsPerPage.value;
+      // return sliced data object to be paginated
+      return props.persons.slice(startIndex, endIndex);
+    });
+
+    //pass this function to pagination component
+    function changePage(pageNumber) {
+      currentPage.value = pageNumber;
+    }
+
     return {
+      currentPage,
+      displayedItems,
+      itemsPerPage,
+      changePage,
       translateTrueFalse,
     };
   },
