4383d2b355274afd9dd35d63148422e51955c8cb
Charlon Palacay <charlon@gmail.com>
2024-02-23 15:46:00 -0800

    Fixes vitest config.

---
+++ vitest.config.js
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [Vue()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      reporter: ["text", "json", "html", "lcov"],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./compass_vue", import.meta.url)),
+    },
+  },
+});
