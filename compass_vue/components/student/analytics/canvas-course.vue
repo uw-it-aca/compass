6dbb793645288c236bede95bc118cc3bc6dfe4b0
Leila Li <ml253@uw.edu>
2024-07-25 12:06:20 -0700

    finished ui styling

--- compass_vue/components/student/analytics/canvas-course.vue
+++ compass_vue/components/student/analytics/canvas-course.vue
@@ -1,19 +1,77 @@
 <template>
   <div class="p-4" style="background-color: #fafafa; border-radius: 10px">
-    <div>
+    <div class="d-flex">
       <h5>Canvas Analytics</h5>
-      <i class="bi bi-info-circle-fill me-0"></i>
+      <a
+        tabindex="0"
+        role="button"
+        class=""
+        data-bs-toggle="popover"
+        data-bs-trigger="focus"
+        data-bs-placement="top"
+        title="Canvas Analytics"
+        data-bs-content="Grade represents the student’s grade in Canvas relative to her classmates up to this point in the quarter. Assignment is indicative of how the student is doing relative to her classmates with regards to the status of assignments. "
+      >
+        <i class="bi bi-info-circle-fill ms-2"></i>
+      </a>
     </div>
     <div class="d-flex pt-3" style="height: 200px">
-      <div class="d-flex justify-content-between w-25">
-        <div class="pt-2">
+      <div class="d-flex w-25">
+        <div class="pt-2 w-100">
           <ul
             class="p-0 fs-7 text-secondary"
             style="list-style-type: none; line-height: 40px"
           >
-            <li>Grade</li>
-            <li>Assignment</li>
-            <li>Activty</li>
+            <!-- if score increased compared to last week, arrow up. if score decreased, arrow down. If score did not change, no icons. -->
+            <li>
+              <div class="d-flex justify-content-between">
+                Grade
+                <div>
+                  40%
+                  <i
+                    style="color: #c12c2c"
+                    class="bi bi-arrow-down-circle-fill ms-2 me-1"
+                  ></i>
+                  <i
+                    style="color: #289026"
+                    class="bi bi-arrow-up-circle-fill ms-2 me-1"
+                  ></i>
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <div class="d-flex justify-content-between">
+                Assignment
+                <div>
+                  30%
+                  <i
+                    style="color: #c12c2c"
+                    class="bi bi-arrow-down-circle-fill ms-2 me-1"
+                  ></i>
+                  <i
+                    style="color: #289026"
+                    class="bi bi-arrow-up-circle-fill ms-2 me-1"
+                  ></i>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="d-flex justify-content-between">
+                Activity
+                <div>
+                  20%
+                  <i
+                    style="color: #c12c2c"
+                    class="bi bi-arrow-down-circle-fill ms-2 me-1"
+                  ></i>
+                  <i
+                    style="color: #289026"
+                    class="bi bi-arrow-up-circle-fill ms-2 me-1"
+                  ></i>
+                </div>
+              </div>
+            </li>
           </ul>
         </div>
         <div class="vr mx-3"></div>
@@ -21,11 +79,26 @@
       <div class="flex-fill">
         <Line :data="chartData" :options="chartOptions" />
       </div>
+      <div>
+        <ul class="p-0 fs-8 text-secondary" style="list-style-type: none">
+          <li>
+            <i style="color: #4b2e83" class="bi bi-circle-fill me-2"></i>Grade
+          </li>
+          <li>
+            <i style="color: #4c7286" class="bi bi-circle-fill me-2"></i
+            >Assignment
+          </li>
+          <li>
+            <i style="color: #ab9765" class="bi bi-circle-fill me-2"></i>Activty
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { Popover } from "bootstrap";
 import { Line } from "vue-chartjs";
 import {
   Chart as ChartJS,
@@ -85,10 +158,7 @@ export default {
         maintainAspectRatio: false,
         plugins: {
           legend: {
-            position: "right",
-            labels: {
-              usePointStyle: true,
-            },
+            display: false,
           },
         },
         scales: {
