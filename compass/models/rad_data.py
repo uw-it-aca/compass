62c9acef037e52753d9059499503bd2cc0b5647c
Stephen De Vight <devights@uw.edu>
2024-10-16 11:16:35 -0700

    round scores and convert signin

--- compass/models/rad_data.py
+++ compass/models/rad_data.py
@@ -85,23 +85,15 @@ class CourseAnalyticsScores(models.Model):
 
     def json_data(self):
         return {'activity_score':
-                self.convert_score_range(self.activity_score),
+                convert_score_range(self.activity_score),
                 'assignment_score':
-                    self.convert_score_range(self.assignment_score),
+                    convert_score_range(self.assignment_score),
                 'grade_score':
-                    self.convert_score_range(self.grade_score),
+                    convert_score_range(self.grade_score),
                 'prediction_score':
-                    self.convert_score_range(self.prediction_score),
+                    convert_score_range(self.prediction_score),
                 'week_id':
                     self.week.week}
-
-    @staticmethod
-    def convert_score_range(old_value):
-        old_min, old_max = -5.0, 5.0
-        new_min, new_max = 0, 100
-        new_value = ((old_value - old_min) / (old_max - old_min)) * (
-            new_max - new_min) + new_min
-        return new_value
 
     def is_alert_status(self):
         return (self.activity_score * 20 < 60 or
@@ -203,7 +195,8 @@ class StudentSigninAnalytics(models.Model):
         for signin in signins:
             year_data = json_data.setdefault(signin.week.year, {})
             quarter_data = year_data.setdefault(signin.week.quarter, {})
-            quarter_data[signin.week.week] = signin.signin_score * 20
+            quarter_data[signin.week.week] = convert_score_range(
+                signin.signin_score * 20)
 
         return json_data
 
@@ -265,3 +258,11 @@ class RADWeek(models.Model):
         return {'year': term['year'] - 1, 'quarter': 'autumn'} if (
             term['quarter'] == 'winter') \
             else {'year': term['year'], 'quarter': quarters[index - 1]}
+
+
+def convert_score_range(old_value):
+    old_min, old_max = -5.0, 5.0
+    new_min, new_max = 0, 100
+    new_value = ((old_value - old_min) / (old_max - old_min)) * (
+        new_max - new_min) + new_min
+    return round(new_value)
