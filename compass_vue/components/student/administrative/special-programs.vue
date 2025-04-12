3472a8d4638c7e21d59483f86eb8e4e2a0aa500d
Charlon Palacay <charlon@gmail.com>
2025-04-11 18:39:14 -0700

    Fixes special program fetch error.

--- compass_vue/components/student/administrative/special-programs.vue
+++ compass_vue/components/student/administrative/special-programs.vue
@@ -109,14 +109,14 @@ export default {
           this.programData = response;
         })
         .catch((error) => {
-          if (error.response.status === 404) {
+          if (error.response === 404) {
             this.programData = { program_date: null };
           } else {
             console.log(
               "Cannot get special program metadata for " +
                 this.person.student.system_key +
                 ": " +
-                error.response
+                error
             );
           }
         });
