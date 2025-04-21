c5eba17705a157d98dc0a439a75a6285e7bd583a
Jim Laney <jlaney@uw.edu>
2025-04-21 07:06:42 -0700

    add access group for tests

--- compass/tests/views/api/test_affiliations.py
+++ compass/tests/views/api/test_affiliations.py
@@ -2,11 +2,16 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from compass.tests import ApiTest
-from compass.models import Affiliation
+from compass.models import AccessGroup, Affiliation
 from mock import patch
 
 
 class AffiliationAPITest(ApiTest):
+    def setUp(self):
+        super().setUp()
+        ag = AccessGroup(name="Test Group", access_group_id="u_astra_group1")
+        ag.save()
+
     @patch('compass.dao.group.is_member_of_group')
     def test_get(self, mock_is_member):
         mock_is_member.return_value = True
