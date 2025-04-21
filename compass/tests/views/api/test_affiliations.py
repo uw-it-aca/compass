ffdb483ce457fc99d69800990855d1e08c7294ad
Jim Laney <jlaney@uw.edu>
2025-04-21 07:00:44 -0700

    fix tests for affiliation api

--- compass/tests/views/api/test_affiliations.py
+++ compass/tests/views/api/test_affiliations.py
@@ -3,14 +3,17 @@
 
 from compass.tests import ApiTest
 from compass.models import Affiliation
+from mock import patch
 
 
 class AffiliationAPITest(ApiTest):
-    def test_get(self):
+    @patch('compass.dao.group.is_member_of_group')
+    def test_get(self, mock_is_member):
+        mock_is_member.return_value = True
         response = self.get_response("access_group_affiliations", "jadviser")
         self.assertEqual(response.status_code, 200, "OK")
         self.assertEqual(response.data, [])
 
+        mock_is_member.return_value = False
         response = self.get_response("access_group_affiliations", "javerage")
         self.assertEqual(response.status_code, 401, "Unauthorized")
-        self.assertEqual(response.data, [])
