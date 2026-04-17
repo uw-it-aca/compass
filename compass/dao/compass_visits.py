6ee14308b65fdcac0f64af8cc67da4209647390f
Stephen De Vight <devights@uw.edu>
2026-04-17 16:47:52 -0700

    compass visits dao

---
+++ compass/dao/compass_visits.py
@@ -0,0 +1,68 @@
+# Copyright 2026 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from uw_compass_visits import CompassVisits
+from restclients_core.exceptions import DataFailureException
+
+
+def get_visits_for_student(netid):
+    # TODO: Switch to syskey
+    """
+    Returns a list of visits for the given student netid.
+    """
+    visits = None
+    try:
+        visits = CompassVisits().get_visits_for_student(netid)
+    except DataFailureException as ex:
+        # If the student doesn't have any visits, return an empty list
+        visits = []
+    return visits
+
+
+def get_admin_visit_list():
+    """
+    Returns a list of all visits for admin users.
+    """
+    visits = []
+    try:
+        visits = CompassVisits().get_visit_admin_list()
+    except DataFailureException as ex:
+        # If there are no visits, return an empty list
+        visits = []
+    return visits
+
+
+def get_visit_options():
+    """
+    Returns a list of visit options.
+    """
+    options = []
+    try:
+        options = CompassVisits().get_visit_options()
+    except DataFailureException as ex:
+        # If there are no visit options, return an empty list
+        options = []
+    return options
+
+
+def admin_create_visit(visit_data):
+    """
+    Creates a visit with the given visit data.
+    """
+    return CompassVisits().admin_create_visit(visit_data)
+
+
+def admin_update_visit(visit_id, is_verified=False, is_checked_out=False):
+    """
+    Updates a visit with the given visit data.
+    """
+    return CompassVisits().admin_update_visit(visit_id,
+                                              verify=is_verified,
+                                              checkout=is_checked_out)
+
+
+def admin_delete_visit(visit_id):
+    """
+    Deletes a visit with the given visit id.
+    """
+    return CompassVisits().admin_delete_visit(visit_id)
---
+++ compass/tests/dao/test_compass_visits.py
@@ -0,0 +1,75 @@
+# Copyright 2026 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+
+from compass.tests import CompassTestCase
+from compass.dao.compass_visits import (get_visits_for_student,
+                                        get_admin_visit_list,
+                                        get_visit_options,
+                                        admin_create_visit,
+                                        admin_update_visit,
+                                        admin_delete_visit)
+from uw_compass_visits.models import Visit
+from restclients_core.exceptions import DataFailureException
+import datetime
+
+
+class CompassVisitsDaoTest(CompassTestCase):
+    def test_visits_for_student(self):
+        visits = get_visits_for_student("javerage")
+        self.assertIsNotNone(visits)
+        self.assertEqual(len(visits), 2)
+        self.assertEqual(visits[0].id, 1)
+        self.assertEqual(visits[0].student_netid, "javerage")
+        self.assertEqual(visits[0].program_area, "Program Area 3")
+
+        visits = get_visits_for_student("jnovisits")
+        self.assertIsNotNone(visits)
+        self.assertEqual(len(visits), 0)
+
+    def test_get_admin_visit_list(self):
+        visits = get_admin_visit_list()
+        self.assertIsNotNone(visits)
+        self.assertEqual(len(visits), 5)
+        self.assertEqual(visits[0].id, 1)
+        self.assertEqual(visits[0].student_netid, "javerage")
+        self.assertEqual(visits[0].program_area, "Program Area 1")
+
+    def test_get_visit_options(self):
+        options = get_visit_options()
+        self.assertIsNotNone(options)
+        self.assertIn('program_areas', options)
+        self.assertIn('tutoring_options', options)
+        self.assertIn('writing_services', options)
+        self.assertEqual(len(options['program_areas']), 4)
+        self.assertEqual(len(options['tutoring_options']), 3)
+        self.assertEqual(len(options['writing_services']), 2)
+        self.assertEqual(options['program_areas'][0]['id'], 1)
+        self.assertEqual(options['program_areas'][0]['name'],
+                         'Program Area 1')
+
+    def test_admin_create_visit(self):
+        visit = Visit(id=6,
+                      student_netid="jnewvisit",
+                      program_area="Program Area 1",
+                      check_in_date=datetime.datetime(2022, 9, 19, 6, 15, 4),
+                      is_verified=False)
+        with self.assertRaises(DataFailureException):
+            # will fail because the mock dao doesn't support creating visits
+            admin_create_visit(visit)
+
+    def test_admin_update_visit(self):
+        visit = admin_update_visit(1)
+        self.assertIsNotNone(visit)
+        self.assertEqual(visit.id, 1)
+
+        with self.assertRaises(DataFailureException):
+            admin_update_visit(999)
+
+    def test_admin_delete_visit(self):
+        response = admin_delete_visit(1)
+        self.assertIsNotNone(response)
+        self.assertEqual(response.status, 200)
+
+        with self.assertRaises(DataFailureException):
+            admin_delete_visit(999)
