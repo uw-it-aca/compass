f446311aa65a24bffeff712545df5389e0727d22
Stephen De Vight <devights@uw.edu>
2026-08-12 21:49:31 -0700

    test new workflow

--- compass/tests/commands/test_load_rad_data.py
+++ compass/tests/commands/test_load_rad_data.py
@@ -19,19 +19,16 @@ class TestLoadRadData(CompassTestCase):
     def test_first_load(self):
         call_command('load_rad_data')
 
-        self.assertEqual(RADWeek.objects.count(), 1)
-        week = RADWeek.objects.first()
-        self.assertEqual(week.year, 2024)
-        self.assertEqual(week.quarter, 'spring')
-        self.assertEqual(week.week, 6)
+        # bucket-driven: all fixture files are imported in a single run
+        self.assertEqual(RADImport.objects.count(), 11)
+        self.assertEqual(
+            RADImport.objects.filter(
+                import_status=RADImport.SUCCESS).count(), 11)
 
-        self.assertEqual(RADImport.objects.count(), 1)
-        rad_import = RADImport.objects.first()
-        self.assertEqual(rad_import.week, week)
-        self.assertEqual(rad_import.import_status, RADImport.SUCCESS)
-
-        self.assertEqual(CourseAnalyticsScores.objects.count(), 4)
-        scores = CourseAnalyticsScores.objects.all()
+        # spot-check data for a specific week
+        week = RADWeek.objects.get(year=2024, quarter='spring', week=6)
+        scores = CourseAnalyticsScores.objects.filter(week=week)
+        self.assertEqual(scores.count(), 4)
         self.assertEqual(scores[0].course, 'TRAIN 100 A')
         self.assertEqual(scores[0].prediction_score, 0.0)
         self.assertEqual(scores[2].prediction_score, 1.0)
@@ -57,10 +54,15 @@ class TestLoadRadData(CompassTestCase):
     def test_load_next_week(self):
         call_command('load_rad_data', week='2024-spring-week-5')
         call_command('load_rad_data')
-        self.assertEqual(RADWeek.objects.count(), 2)
-        week = RADWeek.objects.last()
-        self.assertEqual(week.week, 6)
-        scores = CourseAnalyticsScores.objects.all()
+        # second call imports all remaining unimported files
+        self.assertEqual(RADImport.objects.count(), 11)
+        week_5 = RADWeek.objects.get(year=2024, quarter='spring', week=5)
+        week_6 = RADWeek.objects.get(year=2024, quarter='spring', week=6)
+        self.assertEqual(
+            RADImport.objects.get(week=week_6).import_status,
+            RADImport.SUCCESS)
+        scores = CourseAnalyticsScores.objects.filter(
+            week__in=[week_5, week_6])
         self.assertEqual(len(scores), 6)
 
     def test_reload(self):
